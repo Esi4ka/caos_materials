468d71a4eac9051ce567d488a4181e114cc75ef8
Esya <56122211+Esi4ka@users.noreply.github.com>
2023-02-04 23:16:33 +0300

    Update example_2.c

--- example_2.c
+++ example_2.c
@@ -7,7 +7,7 @@ void *thread_func(void *param); /* потоковая функция */
 
 int main(int argc, char *argv[]) {
   pthread_t tid; /* идентификатор потока */
-  pthread_attr_t attr; /* отрибуты потока */
+  pthread_attr_t attr; /* aтnрибуты потока */
 
 if (argc != 2) {
   fprintf(stderr,"usage: progtest <integer value>\n");
@@ -21,12 +21,14 @@ if (atoi(argv[1]) < 0) {
 
 /* получаем дефолтные значения атрибутов */
   pthread_attr_init(&attr);
+  
+  /* ставлю минимальный размер стека у потока*/
+  pthread_attr_setstacksize(&attr, PTHREAD_STACK_MIN);
 
 /* создаем новый поток */
   pthread_create(&tid,&attr,potok,argv[1]);
 
 /* ждем завершения исполнения потока */
-  // здесь второй аргумент -- это указатель на возвращаемое значение функции, если оно нам интресно
   pthread_join(tid,NULL);
   printf("count = %d\n",count);
 }
@@ -37,9 +39,11 @@ void *thread_func(void *param) {
   count = 0;
 
   if (upper > 0) {
-    for (i = 1; i <= upper; i++)
+    for (i = 1; i <= upper; ++i)
     count += i;
   }
 
   pthread_exit(0);
 }
+
+//что же делает этот код?
